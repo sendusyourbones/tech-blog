5f2d2a5f84ad49a63eb75ecb1888cde59779093e
sendusyourbones <beckee.roos@gmail.com>
2023-05-10 14:07:07 -0700

    Init repo with packages, server setup, and schema

---
+++ server.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const express = require('express');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+
+const hbs = exphbs.create({});
+
+const sequelize = require('./config/connection');
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+const sessionConfig = {
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+};
+
+app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(session(sessionConfig));
+
+sequelize.sync({ force: false }).then(() =>{
+    app.listen(PORT, () => {
+        console.log(`App listening on port ${PORT}!`);
+    });
+});
